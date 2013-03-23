324ae55c02a276f73234c0ea0f19af869fe54a79
root <root@localhost.localdomain>
2013-03-23 12:07:55 +0900

    ランダムで取得

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -51,32 +51,53 @@ class ContentsController < ApplicationController
 =end
 
     baseurl = "https://maps.googleapis.com/maps/api/place/search/json?"
-    lat = params[:point][:lat].to_s  #緯度
-    lng = params[:point][:lng].to_s  #経度
-    radius = params[:point][:radius].to_s  #探索半径（m）
-    #type = "bar"  #対象
+
+    #lat_s = params[:point][:lat].to_s  #緯度
+    #lng_s = params[:point][:lng].to_s  #経度
+    #radius_s = params[:point][:radius].to_s  #探索半径（m）
+
+    lat_f = params[:point][:lat].to_f  #緯度
+    lng_f = params[:point][:lng].to_f  #経度
+    radius_i = params[:point][:radius].to_i  #探索半径（m）
+
+    type = "bar"  #対象
     sensor = "false"  #場所センサー取得フラグ
-    language = "ja"
+    language = "ja"  #言語
     key = ENV['GOOGLE_API_KEY']  #APIキー
 
-    randomLatlng(params[:point][:lat].to_f,params[:point][:lng].to_f,params[:point][:radius].to_i) 
+    rnd_lat_s = Array.new(5, "")
+    rnd_lng_s = Array.new(5, "")
+    radius_s = (radius_i/2).to_s
     
-    logger.debug(params[:point][:lat].to_s)
-    logger.debug(params[:point][:lng].to_s)
-    logger.debug(params[:point][:radius].to_s)
+    @results = []
+    
+    (0..4).each do |num|
+      rnd_lat_s[num],rnd_lng_s[num] = randomLatlng(lat_f,lng_f,radius_i/2) 
+
+      combine = baseurl + 'location=' + rnd_lat_s[num] + ',' + rnd_lng_s[num]  + '&' + 'radius=' + radius_s + '&' + "language=" + language + '&' + "types=" + type + '&' + "sensor=" + sensor +  '&' + "key=" + key
+      url = combine
+      logger.debug(url)
+      result = open(url) do |file|
+        JSON.parse(file.read)
+      end
+      
+      @results.push(result['results'][rand(result['results'].size)])
+    end
+    
+    @lat_center = lat_f
+    @lng_center = lng_f
+    
 
     #combine = baseurl + 'location=' + lat + ',' + lng  + '&' + 'radius=' + radius + '&' + "types=" + type + '&' + "sensor=" + sensor +  '&' + "key=" + key
-    combine = baseurl + 'location=' + lat + ',' + lng  + '&' + 'radius=' + radius + '&' + "language=" + language + '&' + "sensor=" + sensor +  '&' + "key=" + key
+    #combine = baseurl + 'location=' + lat_s + ',' + lng_s  + '&' + 'radius=' + radius_s + '&' + "language=" + language + '&' + "sensor=" + sensor +  '&' + "key=" + key
 
-    url = combine
+    #url = combine
 
-    logger.debug(url)
+    #result = open(url) do |file|
+    #  JSON.parse(file.read)
+    #end
 
-    result = open(url) do |file|
-      JSON.parse(file.read)
-    end
-
-    @results = result['results']
+    #@results = result['results']
 
     #logger.debug(result)
 
@@ -95,23 +116,38 @@ class ContentsController < ApplicationController
     x1 = lat + distance/lat_d*0.000001
     y1 = lng + distance/lng_d*0.000001
 
+    logger.debug("*****lat-lng*****")
+    logger.debug(lat)
+    logger.debug(lng)
+
+    logger.debug("*****x0-y0-x1-y1*****")
+    logger.debug(x0)
+    logger.debug(y0)
+    logger.debug(x1)
+    logger.debug(y1)
+
     lat_dist = (x1 * 100000) - (x0 * 100000)
     lng_dist = (y1 * 100000) - (y0 * 100000)
     
+    logger.debug("*****lat_dist-lng_dist*****")
     logger.debug(lat_dist.to_i)
     logger.debug(lng_dist.to_i)
     
     lat_rnd = rand(lat_dist.to_i)
     lng_rnd = rand(lng_dist.to_i)
 
+    logger.debug("*****lat_rnd-lng_rnd*****")
     logger.debug(lat_rnd.to_i)
     logger.debug(lng_rnd.to_i)
     
-    lat = lat - (lat_rnd * 0.000001)
-    lng = lng - (lng_rnd * 0.000001)
+    lat = x0 + (lat_rnd * 0.00001)
+    lng = y0 + (lng_rnd * 0.00001)
 
+    logger.debug("*****lat-lng*****")
     logger.debug(lat)
     logger.debug(lng)
+    
+    return lat.to_s,lng.to_s
 
 =begin
     if distance.is_a?(Numeric)
